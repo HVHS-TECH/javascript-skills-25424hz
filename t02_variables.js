8a70fd4f871c3251be183b5f9979bfc759e6cb21
25424hz <25424hz@hvhs.school.nz>
2026-04-29 12:30:56 +1200

    4.29 goes to task3

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,22 @@
-/* Paste your code from task 1 here */
+/****************************
+Header here
+****************************/
+console.log("Running t02_introduction.js")
+
+// Variables below
+let age = 15;
+let dollar = 5000;
+let name = "axolotl";
+let year = 2026
+
+/****************************
+Main Code
+****************************/
+console.log("Hello, "+ name);
+console.log("As of "+ year +" you are " + age,"years old")
+console.log("Congratulations! You have "+ dollar +" dollars")
+
+/***************************
+function
+****************************/
+
--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,33 @@
-/* Paste your code from task 2 here */
+/****************************
+Header here
+****************************/
+console.log("Running t03_introduction.js")
+
+// Variables below
+let age = 15;
+let dollar = 5000;
+let name = "axolotl";
+let year = 2026
+var answer;
+
+/****************************
+Main Code
+****************************/
+console.log("Hello, "+ name);
+console.log("As of "+ year +" you are " + age,"years old");
+answer = year - age;
+console.log("If you have celebrated your birthday this year, you were born in " + answer);
+answer = answer - 1;
+console.log("If you haven't celebrated your birthday this year, you were born in " + answer);
+answer = age + 10;
+console.log("In ten years, you'll be " + answer + " years old");
+console.log("Congratulations! You have "+ dollar +" dollars");
+answer = dollar / 2;
+console.log("You spend half of your money, now you have "+ answer);
+answer = answer + 3;
+console.log("Then you picked $3 on the ground, now you have " + answer);
+
+/***************************
+function
+****************************/
+
