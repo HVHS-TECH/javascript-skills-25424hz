847d9d5d2148454579df639507be3fc590209267
25424hz <25424hz@hvhs.school.nz>
2026-04-30 13:30:23 +1200

    task 4 & 5

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,36 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Header here
+****************************/
+console.log("Running t05_introduction.js")
+
+// Variables below
+let age = 15;
+let dollar = 5000;
+let name = "axolotl";
+let year = 2026
+var answer;
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+
+/****************************
+Main Code
+****************************/
+OUTPUT.innerHTML = "<br>"
+OUTPUT.innerHTML += "<h2>Added by JavaScript</h2>"
+OUTPUT.innerHTML += "<p>Hello, " + name + "</p>";
+OUTPUT.innerHTML += "<p>As of " + year + ", you are " + age + " years old.</p>";
+answer = year - age;
+OUTPUT.innerHTML += "If you have celebrated your birthday this year, you were born in " + answer + "</p>";
+answer = answer - 1;
+OUTPUT.innerHTML += "If you haven't celebrated your birthday this year, you were born in " + answer + "</p>";
+answer = age + 10;
+OUTPUT.innerHTML += "In ten years, you'll be " + answer + " years old</p>";
+OUTPUT.innerHTML += "Congratulations! You have " + dollar + " dollars</p>";
+answer = dollar / 2;
+OUTPUT.innerHTML += "You spend half of your money, now you have $" + answer + "</p>";
+answer = answer + 3;
+OUTPUT.innerHTML += "Then you picked $3 on the ground, now you have $" + answer + "</p>";
+
+/***************************
+function
+****************************/
+
